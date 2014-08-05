50e25ad9f5d7bde5819445550ec6e3057bdc4103
Torbjørn Marø <torbjorn.maro@gmail.com>
2014-08-05 15:06:20 +0200

    Bumped version number to 1.1.0

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,31 @@
 var gateway = require('./../gateway.js');  
+gateway.sendsms({
+  user: "", 
+  password: "", 
+  sender: "test", 
+  receivers: [ "4711111111" ], 
+  message: "A test message", 
+  'done': function(status) { 
+    console.log("Message sent with status: " + JSON.stringify(status));
+    process.exit(0); 
+  }, 
+  'error': function(error) {
+    console.log("Error sending: " + error);
+    process.exit(1);
+  }
+});
+
+
+/**
+
+  OLD STYLE (v 1.0.1)
+  -------------------
+
 gateway.sendsms(
   "user", 
-  "pass", 
-  "test", 
-  [ "4799999999" ], 
+  "passwd", 
+  "sender", 
+  [ "4711111111" ], 
   "A test message", 
   function(status) { 
     console.log("Message sent with status: " + JSON.stringify(status));
@@ -14,3 +36,4 @@ gateway.sendsms(
     process.exit(1);
   }
 );
+*/
